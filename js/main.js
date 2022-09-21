7f6febca2a8ed4c7511854cb47ba9c1f2175d223
tnalss <indoubt@naver.com>
2022-09-21 12:59:02 +0900

    carousel study

--- js/main.js
+++ js/main.js
@@ -3,17 +3,42 @@ window.addEventListener("DOMContentLoaded", function() {
 	const POPUPOUTER = document.querySelector("#popup");
 	const BODY = document.querySelector("#index-body");
 	const CLOSE = document.querySelector("#close");
+	
+	const PREV = document.querySelector(".prev")
+	const NEXT = document.querySelector(".next");
+	const POSTS = document.querySelector("#posts");
+	
+	let postIndex=0;
+	
 	function showAfterLoad() {
+		
 		//console.log(popupOuter);
 		//console.dir(popupOuter);
-		POPUPOUTER.style.display = "block";
 		//console.dir(BODY);
+		POPUPOUTER.style.display = "block";
 		BODY.style.overflow = "hidden";
-		/* 
-		$("#popup").css("display","block"); */
 	}
 	CLOSE.addEventListener("click", function() {
 		POPUPOUTER.style.display = "none";
 		BODY.style.overflow = "auto";
 	})
+
+	NEXT.addEventListener("click", function(){
+		postIndex++;
+		if(postIndex>=3) postIndex=0;		
+		let coords = -920 * postIndex + "px";
+		POSTS.style.marginLeft=coords;
+	})
+	
+	PREV.addEventListener("click", function(){
+		
+		if(postIndex<0) postIndex=2;		
+		let coords = -920 * postIndex + "px";
+		POSTS.style.marginLeft=coords;
+		postIndex--;
+				
+	})
+	
+
+	
 })
